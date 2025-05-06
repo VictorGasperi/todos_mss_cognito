c2eb18e63827ec69a8b4a762b8b2a7fa0471fe05
Victor Gasperi <victorgasperi@gmail.com>
2025-05-06 00:24:45 -0300

    test(user): entidade user testada

--- tests/shared/domain/entities/user.test.ts
+++ tests/shared/domain/entities/user.test.ts
@@ -1,70 +1,57 @@
 import { User } from '../../../../src/shared/domain/entities/user'
-import { STATE } from '../../../../src/shared/domain/enums/state_enum'
 import { EntityError } from '../../../../src/shared/helpers/errors/domain_errors'
-import { describe, it, expect } from 'vitest'
 
 describe('[User Entity Tests]', () => {
-  it('Assert User Entity is correct at all', () => {
+  it('should create a valid User entity', () => {
     const user = new User({
-      id: 0,
-      name: 'Rodrigo Siqueira',
-      email: 'rodrigo.dsiqueira1@gmail.com',
-      state: STATE.PENDING
-    })
+      id: 'f8d9d4b3-b15a-4fcd-b6ea-xxxxxxxxxxxx',
+      name: 'Victor Gasperi',
+      email: 'email1@email.com',
+      password: 'minhaSenhaForte'
+    });
 
-    expect(user).toBeInstanceOf(User)
-  })
+    expect(user).toBeInstanceOf(User);
+  });
 
-  it('Assert User Entity has an error when name is invalid', () => {
+  it('should throw an EntityError when name is invalid', () => {
     expect(() => {
       new User({
-        id: 0,
+        id: 'f8d9d4b3-b15a-4fcd-b6ea-xxxxxxxxxxxx',
         name: '',
-        email: 'rodrigo.dsiqueira1@gmail.com',
-        state: STATE.PENDING
-      })
-    }).toThrowError(EntityError)
+        email: 'email1@email.com',
+        password: 'minhaSenhaForte'
+      });
+    }).toThrow(EntityError);
+
     expect(() => {
       new User({
-        id: 0,
+        id: 'f8d9d4b3-b15a-4fcd-b6ea-xxxxxxxxxxxx',
         name: '',
-        email: 'rodrigo.dsiqueira1@gmail.com',
-        state: STATE.PENDING
-      })
-    }).toThrowError('Field props.name is not valid')
-  })
-  it('Assert User Entity has an error when email is invalid', () => {
-    expect(() => {
+        email: 'email1@email.com',
+        password: 'minhaSenhaForte'
+      });
+    }).toThrow('Field User name is not valid');
+  });
+
+  it('should thorw an EntityError when email is invalid', () => {
+    expect( () => {
       new User({
-        id: 0,
-        name: 'Rodrigo Diana Siqueira',
-        email: 'rodrigo.dsiqueira1',
-        state: STATE.PENDING
+        id: 'f8d9d4b3-b15a-4fcd-b6ea-xxxxxxxxxxxx',
+        name: 'Victor',
+        email: '',
+        password: 'senhaforte123'
       })
-    }).toThrowError(EntityError)
-    expect(() => {
+    }).toThrow('Field User email is not valid')
+  });
+
+  it('should thorw an EntityError when password is invalid', () => {
+    expect( () => {
       new User({
-        id: 0,
-        name: 'Rodrigo Diana Siqueira',
-        email: 'rodrigo.dsiqueira1',
-        state: STATE.PENDING
+        id: 'f8d9d4b3-b15a-4fcd-b6ea-xxxxxxxxxxxx',
+        name: 'Victor',
+        email: 'email1@email.com',
+        password: ''
       })
-    }).toThrowError('Field props.email is not valid')
-  })
-  it('Assert User Entity has errors with state not passed', () => {
-    expect(() => {
-      new User({
-        id: 0,
-        name: 'Rodrigo Diana Siqueira',
-        email: 'rodrigo.dsiqueira1@gmail.com'
-      })
-    }).toThrowError(EntityError)
-    expect(() => {
-      new User({
-        id: 0,
-        name: 'Rodrigo Diana Siqueira',
-        email: 'rodrigo.dsqueira1@gmail.com'
-      })
-    }).toThrowError('Field props.state is not valid')
-  })
-})
+    }).toThrow('Field User password is not valid')
+  });
+});
