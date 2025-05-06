85640eb603c48aa53aabb3261fe91e5c290b5633
Victor Gasperi <victorgasperi@gmail.com>
2025-05-06 00:28:26 -0300

    feat(repo): interface dos repositorios feita

--- src/shared/domain/repositories/user_repository_interface.ts
+++ src/shared/domain/repositories/user_repository_interface.ts
@@ -1,10 +1,12 @@
 import { User } from '../entities/user'
 
 export interface IUserRepository {
-  getUser(id: number): Promise<User>
   getAllUsers(): Promise<User[]>
   createUser(user: User): Promise<User>
-  updateUser(id: number, newName: string, newEmail: string): Promise<User>
-  deleteUser(id: number): Promise<User>
-  getUserCounter(): Promise<number>
+  updateUser(id: string, kvp_to_update: {[key: string]: string}): Promise<User>
+  deleteUser(id: string): Promise<User>
+  confirmUserEmail(id: string, code: number): Promise<boolean>
+  loginUser(email: string, password: string): Promise<{ [key: string]: string }>
+  checkToken(token: string): Promise< { [key: string]: string } >
+  refreshToken(refresh_token: string): Promise<{ [key: string]: string }>
 }
