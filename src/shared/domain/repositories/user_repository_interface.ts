0eed46b4e8c311647683af71657c95521848dde7
Victor Gasperi <victorgasperi@gmail.com>
2025-05-07 13:48:09 -0300

    feat(repository): user comecado

--- src/shared/domain/repositories/user_repository_interface.ts
+++ src/shared/domain/repositories/user_repository_interface.ts
@@ -2,10 +2,11 @@ import { User } from '../entities/user'
 
 export interface IUserRepository {
   getAllUsers(): Promise<User[]>
-  createUser(user: User): Promise<User>
-  updateUser(id: string, kvp_to_update: {[key: string]: string}): Promise<User>
-  deleteUser(id: string): Promise<User>
-  confirmUserEmail(id: string, code: number): Promise<boolean>
+  getUserByEmail(email: string): Promise<User>
+  createUser(name: string, email: string, password: string): Promise<User>
+  updateUser(email: string, new_name?: string, new_password?: string): Promise<User>
+  deleteUser(email: string): Promise<User>
+  confirmUserEmail(email: string, code: number): Promise<boolean>
   loginUser(email: string, password: string): Promise<{ [key: string]: string }>
   checkToken(token: string): Promise< { [key: string]: string } >
   refreshToken(refresh_token: string): Promise<{ [key: string]: string }>
--- src/shared/infra/repositories/user_repository_mock.ts
+++ src/shared/infra/repositories/user_repository_mock.ts
@@ -1,67 +1,76 @@
-import { User } from '../../domain/entities/user'
-import { STATE } from '../../domain/enums/state_enum'
-import { IUserRepository } from '../../domain/repositories/user_repository_interface'
-import { NoItemsFound } from '../../helpers/errors/usecase_errors'
+import { User } from "@/shared/domain/entities/user";
+import { IUserRepository } from "@/shared/domain/repositories/user_repository_interface";
+import { DuplicatedItem, NoItemsFound } from "@/shared/helpers/errors/usecase_errors";
+
 
 export class UserRepositoryMock implements IUserRepository {
-  private users: User[] = [
-    new User({
-      id: 1,
-      name: 'user1',
-      email: 'user1@gmail.com',
-      state: STATE.PENDING
-    }),
-    new User({
-      id: 2,
-      name: 'user2',
-      email: 'user2@gmail.com',
-      state: STATE.PENDING
-    }),
-  ]
 
-  getLength(): number {
-    return this.users.length
-  }
+    private users: User[] = [
+        new User({
+            id: "1",
+            name: "Victor Gasperi",
+            email: "email@email.com",
+            password: "pass"
+        }),
+        new User({
+            id: "2",
+            name: "Gasperi Victor",
+            email: "email2@email.com",
+            password: "pass2"
+        }),
+        new User({
+            id: "3",
+            name: "Jose Augusto",
+            email: "email3@email.com",
+            password: "pass3"
+        })
+    ]
 
-  private userCounter: number = 2
-
-  async getUser(id: number): Promise<User> {
-    const user = this.users.find(user => user.id === id)
-    if (!user) {
-      throw new NoItemsFound('id')
+    async getAllUsers(): Promise<User[]> {
+        return this.users
     }
-    return user
-  }
 
-  async getAllUsers(): Promise<User[]> {
-    return this.users
-  }
+    async getUserByEmail(email: string): Promise<User> {
+        for (const user of this.users) {
+            if ( user.email === email) return user
+        }
 
-  async createUser(user: User): Promise<User> {
-    this.users.push(user)
-    return user
-  }
-
-  async updateUser(id: number, newName: string, newEmail: string): Promise<User> {
-    const user = this.users.find(user => user.id === id)
-    if (!user) {
-      throw new NoItemsFound('id')
+        throw new NoItemsFound('User email')
     }
-    user.setName = newName
-    user.setEmail = newEmail
-    return user
-  }
 
-  async deleteUser(id: number): Promise<User> {
-    const user = this.users.find(user => user.id === id)
-    if (!user) {
-      throw new NoItemsFound('id')
+    async createUser(name: string, email: string, password: string): Promise<User> {
+        
+        for (let user of this.users) {
+            if (user.email === email) throw new DuplicatedItem('user email')
+        }
+
+        const user = new User({
+            id: (this.users.length + 1).toString(),
+            name,
+            email,
+            password
+        });
+
+        this.users.push(user)
+
+        return user
+        
     }
-    this.users = this.users.filter(user => user.id !== id)
-    return user
-  }
 
-  async getUserCounter(): Promise<number> {
-    return this.userCounter
-  }
+    async updateUser(email: string, new_name?: string, new_password?: string): Promise<User> {
+        
+        const user = await this.getUserByEmail(email)
+
+        if (new_name != undefined) {
+            user.setName = new_name
+        }
+
+        if (new_password != undefined) {
+            user.setPassword = new_password
+        }
+
+        return user
+
+    }
+
 }
