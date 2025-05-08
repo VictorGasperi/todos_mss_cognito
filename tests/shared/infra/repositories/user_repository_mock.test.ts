4d9398562278bfeb5b446b24d1edb0799c86657b
Victor Gasperi <victorgasperi@gmail.com>
2025-05-08 18:05:56 -0300

    test(repository): comecando os UT

--- tests/shared/infra/repositories/user_repository_mock.test.ts
+++ tests/shared/infra/repositories/user_repository_mock.test.ts
@@ -1,55 +1,102 @@
-import { describe, it, expect } from 'vitest'
-import { User } from '../../../../src/shared/domain/entities/user'
-import { STATE } from '../../../../src/shared/domain/enums/state_enum'
+import { DuplicateItemException } from '@aws-sdk/client-dynamodb';
+import { User } from '../../../../src/shared/domain/entities/user';
+import { DuplicatedItem, NoItemsFound } from '../../../../src/shared/helpers/errors/usecase_errors';
 import { UserRepositoryMock } from '../../../../src/shared/infra/repositories/user_repository_mock'
+import { isEqual, reject } from 'lodash';
 
-describe('Assert User Repository Mock is correct at all', () => {
-  it('Should create user correctly', async () => {
-    const user = new User({
-      id: 10,
-      name: 'usuario10',
-      email: 'usuario10@gmail.com',
-      state: STATE.PENDING
+
+describe("Assert User Repository Mock is correct", () => {
+
+    it("Should retrieve all users", async () => {
+
+        const repo = new UserRepositoryMock()
+
+        const users = await repo.getAllUsers()
+
+        expect(users.length === 3)
+        expect(users[2].name === "Gasperi Victor")
+
+    });
+
+    it("Should get user by email", async () => {
+
+        const repo = new UserRepositoryMock()
+        const user = await repo.getUserByEmail('email3@email.com')
+
+        expect(user.name === "Jose Augusto")
+
+    });
+
+    it("Shoud throw a NoItemsFound error", async () => {
+
+        const repo = new UserRepositoryMock()
+        
+        await expect(
+            repo.getUserByEmail('emailNaoExistente')
+        )
+        .rejects
+        .toThrow(NoItemsFound)
+
     })
 
-    const repo = new UserRepositoryMock()
-    const lastLength = repo.getLength()
-    await repo.createUser(user)
-    const newLength = repo.getLength()
+    it("Should create a new user", async () => {
 
-    expect(newLength).toEqual(lastLength + 1)
-  })
-  it('Should get user correctly', async () => {
-    const repo = new UserRepositoryMock()
-    const user = await repo.getUser(1)
+        const repo = new UserRepositoryMock()
 
-    expect(user?.id).toEqual(1)
-    expect(user?.name).toEqual('user1')
-    expect(user?.email).toEqual('user1@gmail.com')
-    expect(user?.state).toEqual(STATE.PENDING)
-  })
-  it('Should get all users correctly', async () => {
-    const repo = new UserRepositoryMock()
-    const users = await repo.getAllUsers()
+        const user = await repo.createUser("Yasmin", "yasmin@email.com", "SenhaForte123!")
+        
+        const expected_user = {
+            "user_id": "4",
+            "user_name": "Yasmin",
+            "user_email": "yasmin@email.com"
+        }
 
-    expect(users.length).toEqual(2)
-  })
-  it('Should update user correctly', async () => {
-    const repo = new UserRepositoryMock()
-    
-    const userUpdated = await repo.updateUser(1, 'usuario1', 'usuario1@gmail.com')
+        expect((await repo.getAllUsers()).length === 4)
+        expect(user.toDict()).toEqual(expected_user)
 
-    expect(userUpdated?.id).toEqual(1)
-    expect(userUpdated?.name).toEqual('usuario1')
-    expect(userUpdated?.email).toEqual('usuario1@gmail.com')
-    expect(userUpdated?.state).toEqual(STATE.PENDING)
-  })
-  it('Should delete user correctly', async () => {
-    const repo = new UserRepositoryMock()
-    const lastLength = repo.getLength()
-    await repo.deleteUser(1)
-    const newLength = repo.getLength()
+    });
 
-    expect(newLength).toEqual(lastLength - 1)
-  })
+    it("Should throw a DuplicatedItem", async () => {
+
+        const repo = new UserRepositoryMock()
+        await expect(
+            repo.createUser("Victor Gasperii", "email@email.com", "passs")
+        )
+        .rejects
+        .toThrow(DuplicatedItem)
+
+    })
+
+    it("Should update an user", async () => {
+
+        const repo = new UserRepositoryMock()
+
+        const user = await repo.updateUser("email2@email.com", "Valentin")
+
+        const all_users = await repo.getAllUsers()
+
+        const expected_user = user.toDict()
+
+        expect( all_users[1].toDict() ).toEqual(expected_user)
+
+    });
+
+    it("Should delete an user", async () => {
+
+        const repo = new UserRepositoryMock()
+        const user_deleted = await repo.deleteUser("email2@email.com")
+        const all_users = await repo.getAllUsers()
+
+        expect(all_users.length).toBe(2)
+        expect(user_deleted.name).toEqual("Gasperi Victor")
+
+    });
+
+    it("Should confirm an user email", async () => {
+
+        const repo = new UserRepositoryMock()
+
+
+    })
+
 })
