56b105594746e88fb5546b71f1ee771c250b4ae6
Victor Gasperi <victorgasperi@gmail.com>
2025-05-13 14:06:10 -0300

    feat(modules): user viewmodel done

---
+++ src/modules/create_user/app/create_user_viewmodel.ts
@@ -0,0 +1,24 @@
+import { UserProps } from '@/shared/domain/entities/user'
+
+export class CreateUserViewmodel {
+  private id?: string
+  private name: string
+  private email: string
+
+  constructor(props: UserProps) {
+    this.id = props.id
+    this.email = props.email
+    this.name = props.name
+  }
+
+  toJSON() {
+    return JSON.stringify({
+      user: {
+        id: this.id,
+        name: this.name,
+        email: this.email,
+      },
+      message: 'The user was created successfully'
+    })
+  }
+}
